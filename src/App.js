d5568e38424ed1ea5a98234084d997b11e8558f8
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-13 08:06:52 -0500

    Works on creating counter.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from "react";
-import Parent from "./components/Parent/Parent";
-import Child from "./components/Parent/Child";
-import Child2 from "./components/Parent/Child2";
-import Child3 from "./components/Parent/Child3";
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter/Counter';
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* the children used in other files are whatever you pass down in between the component tags */}
-      <Parent>
-        <h1>Good Morning Class</h1>
-        <div>Hello Back</div>
-      </Parent>
-
-      <Parent>
-        <Child />
-        <Child2 />
-        <Child3 />
-      </Parent>
+    <div>
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
